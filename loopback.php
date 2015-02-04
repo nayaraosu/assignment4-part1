97ca2799c7f449887ddb20e690e03044fce260cf
Ashok Nayar <nayara@onid.oregonstate.edu>
2015-02-03 17:48:16 -0800

    added validation and json object to loopback.php

--- loopback.php
+++ loopback.php
@@ -7,9 +7,30 @@
 </head>
 <body>
    <?php 
-           echo count($_GET);
-
     
+        if (count($_GET) == 0)
+        {
+            $json_array  = array("Type" => "GET", "parameters" => null );
+            $json_obj = json_encode($json_array);
+        }
+        else
+        {
+            $tmp = array();
+            foreach ($_GET as $key => $value) 
+            {
+                if($value == "")
+                {
+                    $tmp[$key] = null;
+                }
+                else
+                {
+                    $tmp[$key] = $value;
+                }
+            }
+            $json_array  = array("Type" => "GET" , "parameters" => $tmp );
+            $json_obj = json_encode($json_array);
+        }
+        echo $json_obj;
    ?> 
 
 </body>
